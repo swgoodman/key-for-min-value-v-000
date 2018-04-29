6cadd2b919872232fb9a15648f5cd89234d47c85
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-29 21:43:48 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(hash)
   lowest_key = nil
   lowest_value = nil
   hash.each do |key, value|
-    if lowest_value == nil || v < lowest_value
+    if lowest_value == nil || value < lowest_value
       lowest_value = value
       lowest_key = key
     end
